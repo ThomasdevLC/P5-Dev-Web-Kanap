a21c67c3b847daa28d40503200e74911868b4c6b
ThomasdevLC <thomas.lecam@gmail.com>
2022-07-19 09:58:14 +0200

    first

---
+++ front/js/product.js
@@ -0,0 +1,109 @@
+let articleId = new URL(location.href).searchParams.get("id");
+
+const CanapData = async () => {
+    await fetch(`http://localhost:3000/api/products/${articleId}`)
+        .then((res) => res.json()
+            .then(function (article) {
+                addArticle(article)
+            }));
+
+}
+
+CanapData()
+
+
+
+const addArticle = (article) => {
+
+    const itemImg = document.getElementById("item__img");
+    const image = document.createElement("img");
+    image.setAttribute('src', article.imageUrl);
+    image.setAttribute('alt', article.altTxt);
+    itemImg.appendChild(image);
+
+
+    document.getElementById("title").textContent = article.name;
+    document.getElementById("price").textContent = article.price;
+    document.getElementById("description").textContent = article.description;
+
+    const options = article.colors
+    const select = document.getElementById("colors")
+    // console.log(options);
+
+
+    // for (i = 0; i < options.length; i++) {
+    //     console.log(options[i]);
+    //     let option = document.createElement("option")
+    //     option.setAttribute('value', options[i])
+    //     option.appendChild(document.createTextNode(options[i]))
+    //     select.appendChild(option)
+    // }
+
+
+    options.forEach(option => {
+
+        let optionColor = document.createElement("option")
+        optionColor.setAttribute('value', option)
+        optionColor.appendChild(document.createTextNode(option))
+        select.appendChild(optionColor)
+    });
+
+}
+
+
+const btn = document.getElementById("addToCart");
+
+btn.addEventListener("click", () => {
+
+    const colorChoice = document.getElementById("colors").value;
+    const inputQty = document.getElementById("quantity").value
+
+    let storageCart = localStorage.getItem("canap_cart")
+
+    let cart
+    if (storageCart)
+        cart = JSON.parse(storageCart)
+    else
+        cart = []
+
+
+    let dataChoice = {
+        id: articleId,
+        color: colorChoice,
+        quantity: Number(inputQty),
+    }
+
+    cart.push(dataChoice)
+
+    localStorage.setItem("canap_cart", JSON.stringify(cart))
+
+
+    if (colorChoice == null || colorChoice === "" || inputQty == null || inputQty == 0) {
+        alert("selectionner une couleur et une quantité")
+    }
+
+
+    window.location.href = "index.html"
+
+});
+
+
+
+// const inputQty = document.getElementById("quantity");
+// let finalQty = "";
+
+// inputQty.addEventListener("input", (e) => {
+//     finalQty = e.target.value;
+//     console.log(finalQty)
+// });
+
+
+// const colorChoice = document.getElementById("colors");
+// let finalColor = "";
+
+// colorChoice.addEventListener("input", (e) => {
+//     finalColor = e.target.value;
+//     console.log(finalColor)
+// })
+
+
--- front/js/script.js
+++ front/js/script.js
@@ -1,19 +1,46 @@
-console.log("helllo")
+init()
 
-
-function init() {
-    // récuperer nos produits
-    // afficher les produits
-    displayProducts()
+async function init() {
+    const products = await fetch('http://localhost:3000/api/products/');
+    createArticle(await products.json());
 
 }
 
-function displayProducts() {
-    let sectionElement = document.getElementById("items")
 
-    let a = document.createElement("a")
-    sectionElement.appendChild(a)
+const createArticle = (canaps) => {
 
-};
+    canaps.forEach((canap) => {
 
-init();
+        const container = document.getElementById("items");
+
+        const card = document.createElement("a");
+        card.setAttribute('href', `./product.html?id=${canap._id}`);
+
+        const article = document.createElement("article");
+        const image = document.createElement("img");
+
+        image.setAttribute('src', canap.imageUrl);
+        image.setAttribute('alt', canap.altTxt);
+
+
+        const title = document.createElement("h3");
+        title.classList.add("productName");
+        const titleText = document.createTextNode(canap.name);
+        title.appendChild(titleText)
+
+        const paragraphe = document.createElement("p");
+        paragraphe.classList.add("productDescription");
+        const descText = document.createTextNode(canap.description);
+        paragraphe.appendChild(descText)
+
+        article.appendChild(title);
+        article.appendChild(image);
+        article.appendChild(paragraphe);
+        card.appendChild(article);
+        container.appendChild(card);
+
+    });
+
+}
+
+
